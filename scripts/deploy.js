604f1577a4acd2306820ee49d2f80e13c34b9f8a
0xdoll <secdollg@gmail.com>
2022-05-01 19:06:34 +0800

    deploy my dapp contracts and verify them on Rinkeby testnet.

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,21 +1,71 @@
+const { ethers } = require("hardhat");
 const hre = require("hardhat");
 
-async function main() {
+const sleep = (waitTimeInMs) => new Promise(resolve => setTimeout(resolve, waitTimeInMs));
+
+async function deployAndVerify() {
   // await hre.run('compile');
 
-  const Contract = await hre.ethers.getContractFactory("Helloer");
-  const contract = await Contract.deploy("Hello, Simon!");
-  await contract.deployed();
+  const RewardToken = await hre.ethers.getContractFactory("RewardToken");
+  const rewardToken = await RewardToken.deploy();
+  await rewardToken.deployed();
 
-  console.log("Contract deployed to:", helloer.address);
+  console.log("rewardToken deployed to:", rewardToken.address);
 
-  // await hre.run("verify:verify", {
-  //   address: contract.address,
-  //   constructorArguments: [
-  //       "Hello, Simon!",
-  //   ],
-  // });
+  const TokenBank = await hre.ethers.getContractFactory("TokenBank");
+  const tokenBank = await TokenBank.deploy();
+  await tokenBank.deployed();
 
+  console.log("tokenBank deployed to:", tokenBank.address);
+
+  const ProxyAdmin = await hre.ethers.getContractFactory("ProxyAdmin");
+  const proxyAdmin = await ProxyAdmin.deploy();
+  await proxyAdmin.deployed();
+  console.log("proxyAdmin deployed to:", proxyAdmin.address);
+
+
+  const MockV3Aggregator = await hre.ethers.getContractFactory("MockV3Aggregator");
+  const mockV3Aggregator = await MockV3Aggregator.deploy(8, ethers.utils.parseUnits("3000", 8));
+  await mockV3Aggregator.deployed();
+  console.log("mockV3Aggregator deployed to:", mockV3Aggregator.address);
+
+  const TransparentUpgradeableProxy = await hre.ethers.getContractFactory("TransparentUpgradeableProxy");
+  const tokenBankProxy = await TransparentUpgradeableProxy.deploy(tokenBank.address, proxyAdmin.address, TokenBank.interface.encodeFunctionData("initialize", [rewardToken.address,]));
+  await tokenBankProxy.deployed();
+  console.log("tokenBankProxy deployed to:", tokenBankProxy.address);
+
+  await sleep(10000);
+
+  await hre.run("verify:verify", {
+    address: tokenBank.address,
+  });
+
+  await hre.run("verify:verify", {
+    address: proxyAdmin.address,
+  });
+
+
+  await hre.run("verify:verify", {
+    address: tokenBankProxy.address,
+    constructorArguments: [
+      tokenBank.address, proxyAdmin.address, TokenBank.interface.encodeFunctionData("initialize", [rewardToken.address,]),
+    ],
+  });
+
+  await hre.run("verify:verify", {
+    address: rewardToken.address,
+  });
+
+  await hre.run("verify:verify", {
+    address: mockV3Aggregator.address,
+    constructorArguments: [
+      8, ethers.utils.parseUnits("3000", 8)
+    ],
+  });
+}
+
+async function main() {
+  await deployAndVerify();
 }
 
 main()
